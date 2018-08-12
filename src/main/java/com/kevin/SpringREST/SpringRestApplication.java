ef2a685394ba325dbe23c167dfb8438f6e4cd057
tawnydev <kevin.matrix@hotmail.fr>
2018-08-12 18:20:12 +0200

    endclean for REST

--- src/main/java/com/kevin/SpringREST/SpringRestApplication.java
+++ src/main/java/com/kevin/SpringREST/SpringRestApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringHateoasApplication {
+public class SpringRestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringHateoasApplication.class, args);
+		SpringApplication.run(SpringRestApplication.class, args);
 	}
 }
